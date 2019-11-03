540e0ed6f58db07403951a632e9726a8e1027d99
Matija <matija.komorcec@gmail.com>
2019-11-03 17:43:23 +0100

    testing this

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'EmptyArray.NET', // Required
+  author: 'Matija Komorcec', // Required
+  description: 'Full-stack Web Developer',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 10, // Required
+  social: {
+    website: 'https://ryanfitzgerald.ca',
+    github: 'https://github.com/ryanfitzgerald',
+    twitter: 'https://twitter.com/ryanafitzgerald',
+    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+  },
+  pathPrefix: '/devblog',
+  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+};
